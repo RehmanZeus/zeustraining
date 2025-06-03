d4d54d90c35c2bfcbc7343eed19f2d32333dbaaf
Rehman Khan <Rehman.Khan@zeuslearning.mumbai>
2025-06-03 16:20:06 +0530

    added form validation, reduced redundant code in script.js

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const dummyForm = document.getElementById("dummy-form");
+
+const validCheck = (fields) => {
+
+    for (const field of fields) {
+
+
+
+        if (!field) {
+            alert(`${field.name} is required`)
+            return false;
+        }
+
+        if (!field.predefined) {
+            if (field.value.length < field.minLength || field.value.length > field.maxLength) {
+                alert(`${field.name} must be minimum of ${field.minLength} and maximum of ${field.maxLength}`)
+                return false;
+            }
+        }
+
+    }
+    return true;
+}
+
+
+const validateFormValues = (dname, cmnt, gender) => {
+
+    const valuesToValidate = [{
+        name: "Name",
+        value: dname,
+        minLength: 3,
+        maxLength: 25,
+        predefined: false
+    }, {
+        name: "Comment",
+        value: cmnt,
+        minLength: 10,
+        maxLength: 250,
+        predefined: false
+    }, {
+        name: "Gender",
+        value: gender,
+        predefined: true
+    }];
+
+    return validCheck(valuesToValidate);
+}
+
+dummyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nameValue = dummyForm.elements["name"].value
+    const commentValue = dummyForm.elements["comments"].value;
+    const genderButtons = document.querySelectorAll('input[name=gender]')
+    let selectedGender;
+
+    for (const x of genderButtons) {
+        if (x.checked) {
+            selectedGender = x.value;
+            break;
+        }
+    }
+    const allGood = validateFormValues(nameValue, commentValue, selectedGender);
+
+    if (allGood) {
+        alert(`Your submitted values ${nameValue} ${commentValue} ${selectedGender}`)
+    } else {
+        alert("Please fix the errors")
+    }
+
+
+
+
+})
