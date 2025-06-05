ef43db58fa537fbc493931bdf3c081c75a784887
RehmanZeus <rehman.khan@zeuslearning.com>
2025-06-05 17:10:56 +0530

    updated ui, and added ui in task 3

--- src/tasks/3/index.js
+++ src/tasks/3/index.js
@@ -1,10 +1,42 @@
 
-const calculateFact = (n) => {
- 
-    for(let i = n - 1; i >= 1; --i){
-        n *= i
+
+
+
+
+const multiply = (arr, n) => {
+    let carry = 0;
+    let result = [];
+    
+    for(let i = arr.length - 1; i>=0; --i){
+        let prod = arr[i] * n + carry;
+        carry = Math.floor(prod/10000);
+        result.unshift(prod % 10000)
     }
-    return n;
+    while(carry > 0){
+        result.unshift(carry%10000);
+        carry = Math.floor(carry/10000);
+    }
+    return result
 }
 
-console.log(calculateFact(10))
+const factorial = (num) => {
+    let arr = [1];
+    for(let n = 1; n <=num ; ++n){
+        arr = multiply(arr,n);
+    }
+
+    
+
+    
+    console.log(arr.join(','))
+
+}
+
+factorial(50000)
+
+// const button = document.getElementById("solve");
+
+// button.addEventListener("click", () => {
+//     const inputValue = document.getElementById("problem").value;
+//     factorial(inputValue);
+// })
